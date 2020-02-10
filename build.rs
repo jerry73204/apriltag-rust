5f017ccd7f284162e11c9b62e16c28ae657ef554
jerry73204 <jerry73204@gmail.com>
2020-02-10 18:04:48 +0800

    Whitelist zarray_* and matd_* types and functions

--- build.rs
+++ build.rs
@@ -12,6 +12,8 @@ fn main() {
         .whitelist_type("image_u8_.*")
         .whitelist_type("image_u8x3_.*")
         .whitelist_type("image_u8x4_.*")
+        .whitelist_type("zarray_.*")
+        .whitelist_type("matd_.*")
         .whitelist_function("apriltag_.*")
         .whitelist_function("tag16h5_.*")
         .whitelist_function("tag25h9_.*")
@@ -24,6 +26,8 @@ fn main() {
         .whitelist_function("image_u8_.*")
         .whitelist_function("image_u8x3_.*")
         .whitelist_function("image_u8x4_.*")
+        .whitelist_function("zarray_.*")
+        .whitelist_function("matd_.*")
         .generate()
         .expect("Unable to generate bindings");
 
