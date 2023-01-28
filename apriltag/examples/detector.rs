29c57ad9f542e43403823e28222d0ce8296835ce
aeon <jerry73204@gmail.com>
2023-01-29 00:50:44 +0800

    Fix compile error on apriltag example code

--- apriltag/examples/detector.rs
+++ apriltag/examples/detector.rs
@@ -1,129 +1,111 @@
-use anyhow::Result;
+use anyhow::{ensure, Context, Error, Result};
+use apriltag::{DetectorBuilder, Family, Image, TagParams};
+use clap::Parser;
+use std::str::FromStr;
 
-#[cfg(feature = "full")]
-mod simple_detector {
-    use anyhow::{ensure, Context, Error, Result};
-    use apriltag::{DetectorBuilder, Family, TagParams};
-    use std::{path::PathBuf, str::FromStr};
-    use structopt::StructOpt;
+/// Simple AprilTag detector.
+#[derive(Debug, Clone, Parser)]
+struct Opts {
+    #[clap(long = "family", default_value = "tag16h5")]
+    /// family name.
+    pub family_name: String,
 
-    #[derive(Debug, Clone, StructOpt)]
-    /// Simple AprilTag detector.
-    struct Opts {
-        #[structopt(long, default_value = "tag16h5")]
-        /// family name.
-        pub family: String,
-        #[structopt(long)]
-        /// optional tag parameters in format "tagsize,fx,fy,cx,cy".
-        pub tag_params: Option<TagParamsArg>,
-        /// input files.
-        pub input_files: Vec<PathBuf>,
-    }
+    #[clap(long)]
+    /// optional tag parameters in format "tagsize,fx,fy,cx,cy".
+    pub tag_params: Option<TagParamsArg>,
 
-    #[derive(Debug, Clone)]
-    struct TagParamsArg {
-        pub tagsize: f64,
-        pub fx: f64,
-        pub fy: f64,
-        pub cx: f64,
-        pub cy: f64,
-    }
+    /// a list of input PNM image files.
+    pub input_files: Vec<String>,
+}
 
-    impl From<TagParamsArg> for TagParams {
-        fn from(arg: TagParamsArg) -> Self {
-            let TagParamsArg {
-                tagsize,
-                fx,
-                fy,
-                cx,
-                cy,
-            } = arg;
+fn main() -> Result<()> {
+    let Opts {
+        family_name,
+        tag_params,
+        input_files,
+    } = Opts::parse();
 
-            Self {
-                tagsize,
-                fx,
-                fy,
-                cx,
-                cy,
+    ensure!(!input_files.is_empty(), "no input files");
+
+    let family: Family = family_name.parse()?;
+    let tag_params: Option<TagParams> = tag_params.map(|params| params.into());
+    let mut detector = DetectorBuilder::new()
+        .add_family_bits(family, 1)
+        .build()
+        .unwrap();
+
+    for path in input_files {
+        let image = Image::from_pnm_file(&path)?;
+        let detections = detector.detect(&image);
+
+        println!("= image {path}");
+
+        detections.into_iter().enumerate().for_each(|(index, det)| {
+            println!("  - detection {index}: {det:#?}");
+            if let Some(tag_params) = &tag_params {
+                let pose = det.estimate_tag_pose(tag_params);
+                println!("  - pose {index}: {pose:#?}");
+                println!("  - isometry {index}: {pose:#?}");
             }
+        });
+    }
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+struct TagParamsArg {
+    pub tagsize: f64,
+    pub fx: f64,
+    pub fy: f64,
+    pub cx: f64,
+    pub cy: f64,
+}
+
+impl From<TagParamsArg> for TagParams {
+    fn from(arg: TagParamsArg) -> Self {
+        let TagParamsArg {
+            tagsize,
+            fx,
+            fy,
+            cx,
+            cy,
+        } = arg;
+
+        Self {
+            tagsize,
+            fx,
+            fy,
+            cx,
+            cy,
         }
     }
+}
 
-    impl FromStr for TagParamsArg {
-        type Err = Error;
+impl FromStr for TagParamsArg {
+    type Err = Error;
 
-        fn from_str(text: &str) -> Result<Self, Self::Err> {
-            let tokens: Vec<_> = text.split(',').collect();
-            ensure!(
-                tokens.len() == 5,
-                r#"tag parameters must be in format "tagsize,fx,fy,cx,cy""#
-            );
+    fn from_str(text: &str) -> Result<Self, Self::Err> {
+        let tokens: Vec<_> = text.split(',').collect();
+        ensure!(
+            tokens.len() == 5,
+            r#"tag parameters must be in format "tagsize,fx,fy,cx,cy""#
+        );
 
-            let values = tokens
-                .into_iter()
-                .map(|token| -> Result<_> {
-                    let value: f64 = token.parse()?;
-                    Ok(value)
-                })
-                .collect::<Result<Vec<_>>>()
-                .with_context(|| format!("failed to parse tag parameters {}", text))?;
-
-            Ok(Self {
-                tagsize: values[0],
-                fx: values[1],
-                fy: values[2],
-                cx: values[3],
-                cy: values[4],
+        let values = tokens
+            .into_iter()
+            .map(|token| -> Result<_> {
+                let value: f64 = token.parse()?;
+                Ok(value)
             })
-        }
+            .collect::<Result<Vec<_>>>()
+            .with_context(|| format!("failed to parse tag parameters {}", text))?;
+
+        Ok(Self {
+            tagsize: values[0],
+            fx: values[1],
+            fy: values[2],
+            cx: values[3],
+            cy: values[4],
+        })
     }
-
-    pub fn _main() -> Result<()> {
-        let Opts {
-            family: family_name,
-            tag_params,
-            input_files,
-        } = Opts::from_args();
-
-        if input_files.is_empty() {
-            eprintln!("no input files");
-            return Ok(());
-        }
-
-        let family: Family = family_name.parse()?;
-        let tag_params: Option<TagParams> = tag_params.map(|params| params.into());
-        let mut detector = DetectorBuilder::new()
-            .add_family_bits(family, 1)
-            .build()
-            .unwrap();
-
-        for path in input_files.into_iter() {
-            let image = image::open(&path)?;
-            let detections = detector.detect(image.to_luma8());
-
-            println!("= image {}", path.display());
-
-            detections.into_iter().enumerate().for_each(|(index, det)| {
-                println!("  - detection {}: {:#?}", index, det);
-                if let Some(tag_params) = &tag_params {
-                    let pose = det.estimate_tag_pose(tag_params);
-                    println!("  - pose {}: {:#?}", index, pose);
-
-                    let isometry = pose.map(|pose| pose.to_isometry());
-                    println!("  - isometry {}: {:#?}", index, isometry);
-                }
-            });
-        }
-        Ok(())
-    }
-}
-
-#[cfg(feature = "full")]
-fn main() -> Result<()> {
-    simple_detector::_main()
-}
-
-#[cfg(not(feature = "full"))]
-fn main() -> Result<()> {
-    panic!(r#"please enable the "full" feature to run the example"#);
 }
